90518de56003ac2cc24cc81f1f81c051d882a730
ajh30117 <90249218+ajh30117@users.noreply.github.com>
2022-07-23 19:34:17 +0800

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,6 @@ module.exports = {
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
         experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
         // Set Variation ID. 0 for original 1,2,3....
         variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
