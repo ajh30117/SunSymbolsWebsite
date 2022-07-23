de70d350a487c948da9890d8593b36bd05853ef5
ajh30117 <90249218+ajh30117@users.noreply.github.com>
2022-07-23 20:06:32 +0800

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,22 +22,8 @@ module.exports = {
         trackingId: "G-YWN6074GS6",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
-        // Setting this parameter is optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
         // defaults to false
         enableWebVitalsTracking: true,
       },
